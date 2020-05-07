6e743e347b91080adc23d7872b57d2c049aad2ee
Katy St Sauveur <katyrogowski@yahoo.com>
2020-05-07 05:34:36 -0700

    Finished book test

--- src/book.js
+++ src/book.js
@@ -1,12 +1,45 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  var charObject = {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  }
+  return charObject
+}
+
+function saveReview(review, reviewArray) {
+  if (!reviewArray.includes(review)) {
+    reviewArray.push(review);
+  }
+}
+
+function calculatePageCount(bookTitle) {
+  return bookTitle.length * 20;
+}
+
+function writeBook(bookTitle, bookCharacter, genre) {
+  return {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: genre,
+  }
+}
+
+function editBook(book) {
+  book.pageCount = book.pageCount * .75
+  return book.pageCount
 }
 
 module.exports = {
   createTitle: createTitle,
-  // buildMainCharacter: buildMainCharacter,
-  // saveReview: saveReview,
-  // calculatePageCount: calculatePageCount,
-  // writeBook: writeBook,
-  // editBook: editBook
+  buildMainCharacter: buildMainCharacter,
+  saveReview: saveReview,
+  calculatePageCount: calculatePageCount,
+  writeBook: writeBook,
+  editBook: editBook
 }
