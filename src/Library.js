32affe8425c5f99994899e3c935bce1f683540cc
Katy St Sauveur <katyrogowski@yahoo.com>
2020-05-08 20:55:35 -0700

    Shelf test complete

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,62 @@
+function createLibrary(name) {
+  var newLibrary = {
+  name: name,
+  shelves: {
+    fantasy: [],
+    fiction: [],
+    nonFiction: [],
+    },
+  }
+  return newLibrary;
+}
 
+function addBook(library, book) {
+  if (book.genre == 'fantasy') {
+    library.shelves.fantasy.push(book);
+  } else if (book.genre == 'nonFiction') {
+    library.shelves.nonFiction.push(book);
+  } else {
+    library.shelves.fiction.push(book);
+  }
+}
+
+function checkoutBook(library, book) {
+  library.shelves.fiction = library.shelves.fiction.filter(checkedOutBook => checkedOutBook.title != book)
+
+  library.shelves.nonFiction = library.shelves.nonFiction.filter(checkedOutBook => checkedOutBook.title != book)
+
+  library.shelves.fantasy = library.shelves.fantasy.filter(checkedOutBook => checkedOutBook.title != book)
+
+  // var allShelves = [library.shelves.fantasy, library.shelves.fiction, library.shelves.nonFiction]
+
+  // var currentShelf = library.shelves.fantasy
+  // currentShelf = currentShelf.filter(checkedOutBook => checkedOutBook.title != book)
+  //
+  // var currentShelf = library.shelves.nonFiction
+  // currentShelf = currentShelf.filter(checkedOutBook => checkedOutBook.title != book)
+  //
+  // var currentShelf = library.shelves.fiction
+  // currentShelf = currentShelf.filter(checkedOutBook => checkedOutBook.title != book)
+
+  return `You have now checked out ${book} from the Denver Public Library`
+  }
+
+
+
+
+// const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+//
+// const result = words.filter(word => word.length > 6);
+
+
+// expected output: Array ["exuberant", "destruction", "present"]
+//loop through the fiction array and search for the title
+//remove from fiction array using filter
+//remove the book from the fiction array
+//return "You have chec..."
 
 module.exports = {
-  // createLibrary: createLibrary,
-  // addBook: addBook,
-  // checkoutBook: checkoutBook
+  createLibrary: createLibrary,
+  addBook: addBook,
+  checkoutBook: checkoutBook
 };
--- src/shelf.js
+++ src/shelf.js
@@ -5,7 +5,28 @@ function shelfBook(title, shelf) {
 }
 
 function unshelfBook(title, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === title) {
+      shelf.splice(i, 1)
+    }
+  }
+}
 
+function listTitles(shelf) {
+  var allTitles = []
+  for (var i = 0; i < shelf.length; i++) {
+    allTitles.push(shelf[i].title);
+ }
+ return allTitles.join(', ');
+}
+
+function searchShelf(shelf, title) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title == title) {
+      return true;
+    }
+  }
+  return false;
 }
 
 
@@ -13,6 +34,6 @@ function unshelfBook(title, shelf) {
 module.exports = {
   shelfBook: shelfBook,
   unshelfBook: unshelfBook,
-  // listTitles: listTitles,
-  // searchShelf: searchShelf
+  listTitles: listTitles,
+  searchShelf: searchShelf
 };
